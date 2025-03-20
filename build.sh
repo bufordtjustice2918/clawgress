b92033fd1ae9a43ee0324b5e35b1f12c8a98c83c
John Estabrook <jestabro@vyos.io>
2025-03-20 09:22:23 -0500

    T7246: update vyos1x-config hash

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#d08be19809a3e1c8413e0d98556273244dc18e77 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#94de6d4cd5c2ca651f75c74c4995db9c69ae5c0c -y'
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#33cc7567d909c776d43ea6698267125ea1ec2f66 -y'
 
 eval `opam config env`
