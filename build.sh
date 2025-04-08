ac281c693948207c1ca5fd73035e8af8a7bba487
John Estabrook <jestabro@vyos.io>
2025-04-08 09:20:16 -0500

    T7302: update hash for backend support for commit dry-run

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=$1
 
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#ccdc583f0f02583771fadae353166ef1a904f4e3 -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#0fb5e2f781725196ae010cf64e4d185a8f5ce721 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#61566db58d2b5f8eacbd1564ba1ae0690ac4b1f6 -y'
 
 eval `opam config env`
 make clean
