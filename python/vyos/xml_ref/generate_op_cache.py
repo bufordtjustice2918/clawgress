e5f3f1b2bb6cadaa7586beff100f83e4ff9159db
John Estabrook <jestabro@vyos.io>
2025-06-13 07:31:40 -0500

    T7561: simplify op-mode-definitions XML cache and add interface methods
    The original implementation of the op-mode XML cache generation resulted
    in a structure that was difficult to use, for example, in documentation
    generation. The source of complication is that, unlike the XML of
    interface-definitions, path names are not unique: the same path may
    occur as both a regular node and as a tag node. Here we simplify the
    underlying structure by enriching path names with type information, thus
    disambiguating paths. An interface to the cache is provided by explicit
    generator and lookup functions.

--- python/vyos/xml_ref/generate_op_cache.py
+++ python/vyos/xml_ref/generate_op_cache.py
@@ -14,10 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import io
 import re
 import sys
-import json
 import glob
+import atexit
 
 from argparse import ArgumentParser
 from os.path import join
@@ -25,23 +26,40 @@ from os.path import abspath
 from os.path import dirname
 from xml.etree import ElementTree as ET
 from xml.etree.ElementTree import Element
+from functools import cmp_to_key
 from typing import TypeAlias
 from typing import Optional
+
+from op_definition import NodeData
+from op_definition import OpKey  # pylint: disable=unused-import # noqa: F401
+from op_definition import OpData  # pylint: disable=unused-import # noqa: F401
+from op_definition import key_name
+from op_definition import key_type
 
 _here = dirname(__file__)
 
 sys.path.append(join(_here, '..'))
-from defaults import directories
+# pylint: disable=wrong-import-position,wrong-import-order
+from defaults import directories  # noqa: E402
 
-from op_definition import PathData
 
-
-xml_op_cache_json = 'xml_op_cache.json'
-xml_op_tmp = join('/tmp', xml_op_cache_json)
 op_ref_cache = abspath(join(_here, 'op_cache.py'))
 
 OptElement: TypeAlias = Optional[Element]
-DEBUG = False
+
+
+# It is expected that the node_data help txt contained in top-level nodes,
+# shared across files, e.g.'show', will reveal inconsistencies; to list
+# differences, use --check-xml-consistency
+CHECK_XML_CONSISTENCY = False
+err_buf = io.StringIO()
+
+
+def write_err_buf():
+    err_buf.seek(0)
+    out = err_buf.read()
+    print(out)
+    err_buf.close()
 
 
 def translate_exec(s: str) -> str:
@@ -74,8 +92,44 @@ def translate_op_script(s: str) -> str:
     return s
 
 
-def insert_node(n: Element, l: list[PathData], path=None) -> None:
-    # pylint: disable=too-many-locals,too-many-branches
+def compare_keys(a, b):
+    match key_type(a), key_type(b):
+        case None, None:
+            if key_name(a) == key_name(b):
+                return 0
+            return -1 if key_name(a) < key_name(b) else 1
+        case None, _:
+            return -1
+        case _, None:
+            return 1
+        case _, _:
+            if key_name(a) == key_name(b):
+                if key_type(a) == key_type(b):
+                    return 0
+                return -1 if key_type(a) < key_type(b) else 1
+            return -1 if key_name(a) < key_name(b) else 1
+
+
+def sort_func(obj: dict, key_func):
+    if not obj or not isinstance(obj, dict):
+        return obj
+    k_list = list(obj.keys())
+    if not isinstance(k_list[0], tuple):
+        return obj
+    k_list = sorted(k_list, key=key_func)
+    v_list = map(lambda t: sort_func(obj[t], key_func), k_list)
+    return dict(zip(k_list, v_list))
+
+
+def sort_op_data(obj):
+    key_func = cmp_to_key(compare_keys)
+    return sort_func(obj, key_func)
+
+
+def insert_node(
+    n: Element, d: dict, path: list[str] = None, parent: NodeData = None
+) -> None:
+    # pylint: disable=too-many-locals,too-many-branches,too-many-statements
     prop: OptElement = n.find('properties')
     children: OptElement = n.find('children')
     command: OptElement = n.find('command')
@@ -124,31 +178,48 @@ def insert_node(n: Element, l: list[PathData], path=None) -> None:
             if comp_scripts:
                 comp_help['script'] = comp_scripts
 
-    cur_node_dict = {}
-    cur_node_dict['name'] = name
-    cur_node_dict['type'] = node_type
-    cur_node_dict['comp_help'] = comp_help
-    cur_node_dict['help'] = help_text
-    cur_node_dict['command'] = command_text
-    cur_node_dict['path'] = path
-    cur_node_dict['children'] = []
-    l.append(cur_node_dict)
+    cur_node_data = NodeData()
+    cur_node_data.name = name
+    cur_node_data.node_type = node_type
+    cur_node_data.comp_help = comp_help
+    cur_node_data.help_text = help_text
+    cur_node_data.command = command_text
+    cur_node_data.path = path
+
+    value = {('node_data', None): cur_node_data}
+    key = (name, node_type)
+
+    cur_value = d.setdefault(key, value)
+
+    if parent and key not in parent.children:
+        parent.children.append(key)
+
+    if (
+        CHECK_XML_CONSISTENCY
+        and cur_value[('node_data', None)] != value[('node_data', None)]
+    ):
+        err_buf.write(
+            f"prev: {cur_value[('node_data', None)]}; new: {value[('node_data', None)]}\n"
+        )
 
     if children is not None:
         inner_nodes = children.iterfind('*')
         for inner_n in inner_nodes:
             inner_path = path[:]
-            insert_node(inner_n, cur_node_dict['children'], inner_path)
+            insert_node(inner_n, d[key], inner_path, cur_node_data)
 
 
-def parse_file(file_path, l):
+def parse_file(file_path, d):
     tree = ET.parse(file_path)
     root = tree.getroot()
     for n in root.iterfind('*'):
-        insert_node(n, l)
+        insert_node(n, d)
 
 
 def main():
+    # pylint: disable=global-statement
+    global CHECK_XML_CONSISTENCY
+
     parser = ArgumentParser(description='generate dict from xml defintions')
     parser.add_argument(
         '--xml-dir',
@@ -156,21 +227,30 @@ def main():
         required=True,
         help='transcluded xml op-mode-definition file',
     )
+    parser.add_argument(
+        '--check-xml-consistency',
+        action='store_true',
+        help='check consistency of node data across files',
+    )
 
     args = vars(parser.parse_args())
 
+    if args['check_xml_consistency']:
+        CHECK_XML_CONSISTENCY = True
+        atexit.register(write_err_buf)
+
     xml_dir = abspath(args['xml_dir'])
 
-    l = []
+    d = {}
 
-    for fname in glob.glob(f'{xml_dir}/*.xml'):
-        parse_file(fname, l)
+    for fname in sorted(glob.glob(f'{xml_dir}/*.xml')):
+        parse_file(fname, d)
 
-    with open(xml_op_tmp, 'w') as f:
-        json.dump(l, f, indent=2)
+    d = sort_op_data(d)
 
     with open(op_ref_cache, 'w') as f:
-        f.write(f'op_reference = {str(l)}')
+        f.write('from vyos.xml_ref.op_definition import NodeData\n')
+        f.write(f'op_reference = {str(d)}')
 
 
 if __name__ == '__main__':
