b8b5f572feb35c54055b4f182b44f275185fd19d
John Estabrook <jestabro@vyos.io>
2025-08-19 15:29:45 -0500

    T7728: T7734: update commit hash for dynamically generate proposed config

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=$1
 
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#b53326dce7dd2dabad2677ad82de4fcd4ea85524 -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#88d926d30b4219c50bbb1167ab58d60c5c5d2bbb -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#2da0981501a9bec8b69d29f1e52c10db90571aa4 -y'
 
 eval `opam config env`
 make clean
