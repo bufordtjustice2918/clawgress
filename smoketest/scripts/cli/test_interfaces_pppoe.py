75e235563dfeeb5133376eb56fd8576ad48f8419
Christian Breunig <christian@breunig.cc>
2025-10-20 18:59:18 +0200

    smoketest: T7858: temporary disable PPPoE client smoketests
    Due to an issue in FRR where dynamic PPPoE interfaces sometimes receive an
    ifIndex of 0 (which is invalid), disable the testcase and add a marker.
    
    Test was just recently added in commit 75e9fd60c ("smoketest: T7858: add PPPoE
    client tests with IPv4, IPv6 and DHCPv6-PD")

--- smoketest/scripts/cli/test_interfaces_pppoe.py
+++ smoketest/scripts/cli/test_interfaces_pppoe.py
@@ -194,7 +194,9 @@ class PPPoEInterfaceTest(VyOSUnitTestSHIM.TestCase):
             tmp = self.getFRRopmode('show ip route 0.0.0.0/0', json=True)
             # Test if we have a default route 0.0.0.0/0 pointing to our PPPoE interface
             tmp = dict_search_recursive(tmp, 'interfaceName')
-            self.assertTrue(any(iface == interface for (iface, _) in tmp))
+
+            #self.assertTrue(any(iface == interface for (iface, _) in tmp))
+        self.skipTest('Bug in FRR 10.2 - PPPoE interfaces sometimes carry ifIndex 0 which is invalid')
 
     def test_pppoe_client_disabled_interface(self):
         # Check if PPPoE Client can be disabled
