vyos/vyos-1x

geoip: T7926: Refactor geoip handling
* Move core logic to separate vyos.geoip module
* Use a sqlite database for storing and querying address ranges by country
* Remove downloaded geoip ranges once loaded into sqlite db
* No longer rebuild geoip sets on each commit unless necessary
* Allows for extensibility using other geoip data vendors

python/vyos/geoip.py
@@ -1,0 +1,175 @@
+
+import csv
+import gzip
+import os
+import sqlite3
+
+from pathlib import Path
+from time import strftime
+
+from vyos.remote import download
+from vyos.template import is_ipv4, render
+from vyos.utils.dict import dict_search_recursive
+from vyos.utils.process import run
+
+nftables_geoip_conf = '/run/nftables-geoip.conf'
+geoip_database_raw = '/usr/share/vyos-geoip/dbip-country-lite.csv.gz'
+geoip_database_path = '/var/cache/vyos/geoip-lookup.db'
+geoip_lock_file = '/var/lock/vyos-geoip.lock'
+
+# Raw data
+
+def geoip_download_dbip():
+    url = 'https://download.db-ip.com/free/dbip-country-lite-{}.csv.gz'.format(strftime("%Y-%m"))
+    try:
+        dirname = os.path.dirname(geoip_database_raw)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+        download(geoip_database_raw, url)
+        return True
+    except:
+        return False
+
+# VyOS database
+
+def db_is_initialised():
+    if not os.path.exists(geoip_database_path):
+        return False
+
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        cur.execute("PRAGMA table_info(users);")
+        rows = cur.fetchall()
+        return len(rows) > 0
+
+def db_initialise():
+    dirname = os.path.dirname(geoip_database_path)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS geoip_ranges (
+                        country_code TEXT NOT NULL,
+                        range TEXT NOT NULL,
+                        version INT NOT NULL
+                    )
+                    """)
+        cur.execute('CREATE INDEX IF NOT EXISTS idx_cc_version ON geoip_ranges(country_code, version)')
+        conn.commit()
+
+def db_import_dbip_ranges(replace=True, delete_file=False):
+    if not os.path.exists(geoip_database_raw):
+        return False
+
+    if not os.path.exists(geoip_database_path):
+        return False
+
+    try:
+        with gzip.open(geoip_database_raw, mode='rt') as csv_fh:
+            reader = csv.reader(csv_fh)
+
+            with sqlite3.connect(geoip_database_path) as conn:
+                cur = conn.cursor()
+
+                if replace:
+                    cur.execute('DELETE FROM geoip_ranges')
+
+                for start, end, code in reader:
+                    version = 4 if is_ipv4(start) else 6
+                    cur.execute('INSERT INTO geoip_ranges (country_code, range, version) VALUES (?, ?, ?)', (code.lower(), f'{start}-{end}', version))
+                conn.commit()
+
+        if delete_file:
+            os.unlink(geoip_database_raw)
+
+        return True
+    except:
+        return False
+
+def db_return_ranges(codes, version):
+    out = []
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        ph = ','.join(['?'] * len(codes))
+        for row in cur.execute(f'SELECT range FROM geoip_ranges WHERE version = ? AND country_code IN ({ph})', [version, *codes]):
+            out.append(row[0])
+    return out
+
+# Update
+
+def geoip_refresh():
+    with GeoIPLock(geoip_lock_file) as lock:
+        if not lock:
+            return True
+
+        if not os.path.exists(nftables_geoip_conf):
+            return False
+
+        result = run(f'nft --file {nftables_geoip_conf}')
+        if result != 0:
+            return False
+
+        return True
+
+def geoip_update(firewall=None, policy=None):
+    with GeoIPLock(geoip_lock_file) as lock:
+        if not lock:
+            print("Script is already running")
+            return False
+
+        if not firewall and not policy:
+            print("Firewall and policy are not configured")
+            return True
+
+        if not os.path.exists(geoip_database_path):
+            print("Running one-time database initialisation")
+            db_initialise()
+            db_import_dbip_ranges()
+
+        firewall_sets = {'v4': {}, 'v6': {}}
+        policy_sets = {'v4': {}, 'v6': {}}
+
+        if firewall:
+            for codes, path in dict_search_recursive(firewall, 'country_code'):
+                version = 6 if path[0] == 'ipv6' else 4
+                vprefix = '6' if version == 6 else ''
+                set_name = f'GEOIP_CC{vprefix}_{path[1]}_{path[2]}_{path[4]}'
+                firewall_sets[f'v{version}'][set_name] = db_return_ranges(codes, version)
+
+        if policy:
+            for codes, path in dict_search_recursive(policy, 'country_code'):
+                version = 6 if path[0] == 'route6' else 4
+                vprefix = '6' if version == 6 else ''
+                set_name = f'GEOIP_CC{vprefix}_{path[0]}_{path[1]}_{path[3]}'
+                policy_sets[f'v{version}'][set_name] = db_return_ranges(codes, version)
+
+        render(nftables_geoip_conf, 'firewall/nftables-geoip-update.j2', {
+            'firewall_sets': firewall_sets,
+            'policy_sets': policy_sets
+        })
+
+        result = run(f'nft --file {nftables_geoip_conf}')
+        if result != 0:
+            print('Error: GeoIP failed to update firewall and/or policy')
+            return False
+
+        return True
+
+# Utility
+
+class GeoIPLock(object):
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        if os.path.exists(self.file):
+            return False
+
+        Path(self.file).touch()
+        return True
+
+    def __exit__(self, exc_type, exc_value, tb):
+        os.unlink(self.file)

bufordtjustice2918/clawgress

python/vyos/geoip.py
@@ -1,0 +1,175 @@
+
+import csv
+import gzip
+import os
+import sqlite3
+
+from pathlib import Path
+from time import strftime
+
+from vyos.remote import download
+from vyos.template import is_ipv4, render
+from vyos.utils.dict import dict_search_recursive
+from vyos.utils.process import run
+
+nftables_geoip_conf = '/run/nftables-geoip.conf'
+geoip_database_raw = '/usr/share/vyos-geoip/dbip-country-lite.csv.gz'
+geoip_database_path = '/var/cache/vyos/geoip-lookup.db'
+geoip_lock_file = '/var/lock/vyos-geoip.lock'
+
+# Raw data
+
+def geoip_download_dbip():
+    url = 'https://download.db-ip.com/free/dbip-country-lite-{}.csv.gz'.format(strftime("%Y-%m"))
+    try:
+        dirname = os.path.dirname(geoip_database_raw)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+        download(geoip_database_raw, url)
+        return True
+    except:
+        return False
+
+# VyOS database
+
+def db_is_initialised():
+    if not os.path.exists(geoip_database_path):
+        return False
+
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        cur.execute("PRAGMA table_info(geoip_ranges);")
+        rows = cur.fetchall()
+        return len(rows) > 0
+
+def db_initialise():
+    dirname = os.path.dirname(geoip_database_path)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS geoip_ranges (
+                        country_code TEXT NOT NULL,
+                        range TEXT NOT NULL,
+                        version INT NOT NULL
+                    )
+                    """)
+        cur.execute('CREATE INDEX IF NOT EXISTS idx_cc_version ON geoip_ranges(country_code, version)')
+        conn.commit()
+
+def db_import_dbip_ranges(replace=True, delete_file=False):
+    if not os.path.exists(geoip_database_raw):
+        return False
+
+    if not os.path.exists(geoip_database_path):
+        return False
+
+    try:
+        with gzip.open(geoip_database_raw, mode='rt') as csv_fh:
+            reader = csv.reader(csv_fh)
+
+            with sqlite3.connect(geoip_database_path) as conn:
+                cur = conn.cursor()
+
+                if replace:
+                    cur.execute('DELETE FROM geoip_ranges')
+
+                for start, end, code in reader:
+                    version = 4 if is_ipv4(start) else 6
+                    cur.execute('INSERT INTO geoip_ranges (country_code, range, version) VALUES (?, ?, ?)', (code.lower(), f'{start}-{end}', version))
+                conn.commit()
+
+        if delete_file:
+            os.unlink(geoip_database_raw)
+
+        return True
+    except:
+        return False
+
+def db_return_ranges(codes, version):
+    out = []
+    with sqlite3.connect(geoip_database_path) as conn:
+        cur = conn.cursor()
+        ph = ','.join(['?'] * len(codes))
+        for row in cur.execute(f'SELECT range FROM geoip_ranges WHERE version = ? AND country_code IN ({ph})', [version, *codes]):
+            out.append(row[0])
+    return out
+
+# Update
+
+def geoip_refresh():
+    with GeoIPLock(geoip_lock_file) as lock:
+        if not lock:
+            return True
+
+        if not os.path.exists(nftables_geoip_conf):
+            return False
+
+        result = run(f'nft --file {nftables_geoip_conf}')
+        if result != 0:
+            return False
+
+        return True
+
+def geoip_update(firewall=None, policy=None):
+    with GeoIPLock(geoip_lock_file) as lock:
+        if not lock:
+            print("Script is already running")
+            return False
+
+        if not firewall and not policy:
+            print("Firewall and policy are not configured")
+            return True
+
+        if not os.path.exists(geoip_database_path):
+            print("Running one-time database initialisation")
+            db_initialise()
+            db_import_dbip_ranges()
+
+        firewall_sets = {'v4': {}, 'v6': {}}
+        policy_sets = {'v4': {}, 'v6': {}}
+
+        if firewall:
+            for codes, path in dict_search_recursive(firewall, 'country_code'):
+                version = 6 if path[0] == 'ipv6' else 4
+                vprefix = '6' if version == 6 else ''
+                set_name = f'GEOIP_CC{vprefix}_{path[1]}_{path[2]}_{path[4]}'
+                firewall_sets[f'v{version}'][set_name] = db_return_ranges(codes, version)
+
+        if policy:
+            for codes, path in dict_search_recursive(policy, 'country_code'):
+                version = 6 if path[0] == 'route6' else 4
+                vprefix = '6' if version == 6 else ''
+                set_name = f'GEOIP_CC{vprefix}_{path[0]}_{path[1]}_{path[3]}'
+                policy_sets[f'v{version}'][set_name] = db_return_ranges(codes, version)
+
+        render(nftables_geoip_conf, 'firewall/nftables-geoip-update.j2', {
+            'firewall_sets': firewall_sets,
+            'policy_sets': policy_sets
+        })
+
+        result = run(f'nft --file {nftables_geoip_conf}')
+        if result != 0:
+            print('Error: GeoIP failed to update firewall and/or policy')
+            return False
+
+        return True
+
+# Utility
+
+class GeoIPLock(object):
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        if os.path.exists(self.file):
+            return False
+
+        Path(self.file).touch()
+        return True
+
+    def __exit__(self, exc_type, exc_value, tb):
+        os.unlink(self.file)